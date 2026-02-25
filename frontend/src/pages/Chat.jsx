df4f175ee89716adef554be7ac82d7d2aa417e55
Pranshu Chauhan <pranshuchauhan149@gmail.com>
2026-02-26 01:07:49 +0530

    done frontend

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -2,20 +2,20 @@ import React from "react"
 
 import ChatMessageInput from "../components/ChatMessageInput"
 import ChatMessages from "../components/ChatMessages"
-import MessageSideBar from "../components/messageSideBar"
+import MessageSideBar from "../components/MessageSideBar"
 
 const Chat = () => {
   return (
-    <div className=" lg:p-16 h-screen flex overflow-hidden">
+    <div className="h-screen flex overflow-hidden">
       
       {/* LEFT SIDEBAR */}
       <MessageSideBar />
 
       {/* RIGHT CHAT AREA */}
-      {/* <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col">
         <ChatMessages />
         <ChatMessageInput />
-      </div> */}
+      </div>
 
     </div>
   )
