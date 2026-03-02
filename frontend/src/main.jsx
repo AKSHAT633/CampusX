630ad73b6cfd97d2b4301c51140bb472cfc91137
pranshu chauhan <152368583+PranshuChauhan149@users.noreply.github.com>
2026-03-02 16:26:49 +0530

    Update server URL to production endpoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store.js";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
 
-export const serverUrl = "http://localhost:4000";
+export const serverUrl = "https://campussync-e49n.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
